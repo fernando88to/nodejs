74cc6a56c8929614eabbe9aa4dd42415f6b89b95
fernando <fernando88to@gmail.com>
2017-05-12 10:39:17 -0300

    Conexão com o mongo feita

--- myapp/app.js
+++ myapp/app.js
@@ -4,26 +4,57 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require("mongoose");
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+
+
+//conexão com o mongo
+
+mongoose.connect('mongodb://localhost/acadtec', function (err) {
+    if(err){
+        console.log("Erro ao conectar no mongo" + err);
+    }else{
+        console.log("Conexão com o mongo efetuada com sucesso");
+    }
+
+});
+
+//var index = require('./routes/index');
+//var users = require('./routes/users');
+
+var session = require('express-session')
+
+var load  = require('express-load')
 
 var app = express();
+load("controllers").then("models").then("routes").into(app);
+
+
+
+//sessão
+app.use(session({
+    secret: "32dsf3423fsdf234",
+    name: "cookie_name",
+    //store: sessionStore, // connect-mongo session store
+    proxy: true,
+    resave: true,
+    saveUninitialized: true
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+//app.use('/', index);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
