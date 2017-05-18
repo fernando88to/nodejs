692f1ebd6665be071d1a95a87add3d185fb5dabf
fernando <fernando88to@gmail.com>
2017-05-18 17:51:06 -0300

    Começando o login

---
+++ myapp/validacoes/autenticacao.js
@@ -0,0 +1,17 @@
+module.exports = function (req, res) {
+    req.assert("email", "E-mail inválido").isEmail();
+    req.assert("password", "Sua senha deve conter 6 a 10 caracteres").len(6,10);
+
+    var validacaoErros = req.validationErrors() || [];
+    if (validacaoErros.length) {
+        validacaoErros.forEach(function (e) {
+            req.flash("erro", e.msg);
+        });
+
+
+        return false;
+
+
+    }
+    return true;
+}
